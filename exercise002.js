cdcc8278de9cfe59a999547665c555b683d10f13
Renata Giera-Dubiel <renatagiera@gmail.com>
2023-11-19 15:55:36 +0000

    fix: Exercise2, calling other APIs

--- exercise002.js
+++ exercise002.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicode = " https://api.thecatapi.com/v1/images/search?limit=10";
 
 const fetchData = async (apiEndPoint) => {
   try {
-    const response = await fetch (apiEndPoint);
+    const response = await fetch(apiEndPoint);
     console.log(response);
     const json = await response.json();
-    console.log(json)
+    console.log(json);
   } catch (error) {
     console.log(error);
   }
